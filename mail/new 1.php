08477702083a641417642cf27e607c8e3caabe64
pankaj patial <panku.patial@gmail.com>
2017-12-25 13:19:59 +0530

    1st
    urban

---
+++ mail/new 1.php
@@ -0,0 +1,13 @@
+<div class="row">
+      <div class="col-md-6"> <img src="img/about.jpg" class="img-responsive"> </div>
+      <div class="col-md-6">
+        <div class="about-text">
+          <h4>Who We Are</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh ante facilisis bibendum.</p>
+          <h4>What We Do</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh ante facilisis bibendum.</p>
+          <h4>Why Choose Us</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at.</p>
+        </div>
+      </div>
+    </div>
